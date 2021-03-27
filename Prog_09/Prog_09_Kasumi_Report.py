b68d96be881b35b6893c30b6f73b2c41162c199b
HelloYeew <me@helloyeew.dev>
2021-03-28 02:54:33 +0700

    ✅ Add weather report (finished)

--- Prog_09/Prog_09_Kasumi_Report.py
+++ Prog_09/Prog_09_Kasumi_Report.py
@@ -5,18 +5,19 @@ import math
 import matplotlib.pyplot as plt
 import json
 
+
 # If you put another line note again I will get Kasumi to 'BanG Dream!' you.
-def read_weather_data():         
-    def read_province_region_data():      
-        region_of = {}   
-        f = open('provinces.txt', encoding='utf-8')      
-        for line in f:       
-            province, region = line.split()   
+def read_weather_data():
+    def read_province_region_data():
+        region_of = {}
+        f = open('provinces.txt', encoding='utf-8')
+        for line in f:
+            province, region = line.split()
             region_of[province] = region
-        return region_of    
-   
-    def get_value(attr):      
-        if attr == None:    
+        return region_of
+
+    def get_value(attr):
+        if attr == None:
             return None
         else:
             return float(attr['Value'])
@@ -28,10 +29,10 @@ def read_weather_data():
     region_of = read_province_region_data()
 
     stations = {}
-    names_in_region = {'N':[], 'E':[], 'W':[], 'S':[], 'C':[], 'NE':[]}
+    names_in_region = {'N': [], 'E': [], 'W': [], 'S': [], 'C': [], 'NE': []}
     for station in json_data['Stations']:
         name = station["StationNameEng"].upper()
-        lat  = get_value(station["Latitude"])
+        lat = get_value(station["Latitude"])
         long = get_value(station["Longitude"])
         temp = get_value(station["Observe"]["Temperature"])
         region = region_of[station["Province"]]
@@ -41,6 +42,7 @@ def read_weather_data():
     date_time = json_data['Header']['LastBuiltDate']
     return date_time, names_in_region, stations
 
+
 def distance(lat1, long1, lat2, long2):
     # Haversine’ formula
     lat1 = math.radians(lat1)
@@ -48,25 +50,27 @@ def distance(lat1, long1, lat2, long2):
     lat2 = math.radians(lat2)
     long2 = math.radians(long2)
     dlat, dlong = lat1 - lat2, long1 - long2
-    x = math.sin(dlat/2)**2 + math.cos(lat1)*math.cos(lat2)*math.sin(dlong/2)**2
-    return 2*math.asin(x**0.5) * 6371 # Radius of the earth in Km.
+    x = math.sin(dlat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlong / 2) ** 2
+    return 2 * math.asin(x ** 0.5) * 6371  # Radius of the earth in Km.
+
 
 def show_all_station_names(stations):
     names = []
     for name in stations:
         names.append(name.title())
     names.sort()
-    for i in range(0, len(names), 5): # five names per line
-        print(', '.join(names[i: i+4]))
+    for i in range(0, len(names), 5):  # five names per line
+        print(', '.join(names[i: i + 4]))
+
 
 def plot_map_info(stations, names, date_time):
-    lats  = [stations[name]['lat' ] for name in names]
+    lats = [stations[name]['lat'] for name in names]
     longs = [stations[name]['long'] for name in names]
-    temps = [str(stations[name]['temp'])+'°c' for name in names]
+    temps = [str(stations[name]['temp']) + '°c' for name in names]
     ruh_m = plt.imread('th_map.jpg')
-    fig, ax = plt.subplots(figsize = (8,7))
+    fig, ax = plt.subplots(figsize=(8, 7))
     bbox = [95.5, 107.0, 5.5, 20.7]
-    ax.set(xlim=bbox[:2], ylim=bbox[2:], title='Thailand '+ date_time)
+    ax.set(xlim=bbox[:2], ylim=bbox[2:], title='Thailand ' + date_time)
     sc = ax.scatter(longs, lats, zorder=1, c='r', s=40)
     for i in range(len(temps)):
         ax.annotate(temps[i], (longs[i], lats[i]), c='k', fontsize=14)
@@ -74,10 +78,10 @@ def plot_map_info(stations, names, date_time):
     ax.imshow(ruh_m, zorder=0, extent=bbox, aspect='equal')
 
     # https://stackoverflow.com/questions/7908636
-    annot = ax.annotate("", xy=(0,0), xytext=(20,20),
-                    textcoords="offset points", fontsize=16,
-                    bbox=dict(boxstyle="round", fc="orange"),
-                    arrowprops=dict(arrowstyle="->"))
+    annot = ax.annotate("", xy=(0, 0), xytext=(20, 20),
+                        textcoords="offset points", fontsize=16,
+                        bbox=dict(boxstyle="round", fc="orange"),
+                        arrowprops=dict(arrowstyle="->"))
     annot.set_visible(False)
 
     def update_annot(ind):
@@ -97,9 +101,11 @@ def plot_map_info(stations, names, date_time):
                 if vis:
                     annot.set_visible(False)
                     fig.canvas.draw_idle()
+
     fig.canvas.mpl_connect("motion_notify_event", hover)
     fig.tight_layout()
     plt.show()
+
 
 def main():
     while True:
@@ -143,9 +149,11 @@ def main():
         else:
             names = names_in_region[region]
             avg_temp = average_temp(stations, names)
-            print('Average temperature =', str(round(avg_temp,1)) + '°c')
+            print('Average temperature =', str(round(avg_temp, 1)) + '°c')
 
     if len(names) > 0: plot_map_info(stations, names, date_time)
+
+
 # If you put another line note again I will get Kasumi to 'BanG Dream!' you. (The 3 line is not the same too. WUT)
 #
 def approx_match(stations, name):
@@ -180,12 +188,13 @@ def approx_match(stations, name):
     # For loop to search
     for station in station_name_list:
         station_list = station.upper()
-        station_list = station_list.replace(' ','')
+        station_list = station_list.replace(' ', '')
         station_list = list(station_list)
         # Use all() function and for loop in one lineto check search keyword in station name
         if all(item in station_list for item in name_list):
             result.append(station)
     return result
+
 
 def top_k_min_temp_stations(stations, K):
     """
@@ -194,26 +203,21 @@ def top_k_min_temp_stations(stations, K):
         :return ลิสต์ของชื่อสถานีที่มีอุณหภูมิต่ำสุด K สถานี ให้เรียงจากซ้ายไปขวาตามสถานีที่มีอุณหภูมิต่ำสุดไปสูงขึ้น
         ในกรณีที่มีอุณหภูมิเท่ากัน ให้เลือกสถานีที่มีชื่อมาก่อนตามลำดับในพจนานุกรม ในกรณีที่มีไม่ถึง K สถานี ก็คืนเท่าที่มี
         """
-    new_dict = {}
-    # Make list of station and temp from dict
     station_name_list = list(stations.keys())
     temp_list = []
     for name in station_name_list:
         temp_list.append(stations[name]["temp"])
-    # Make a new dict
+    # Make a dict with station and temp
+    temp_dict = {}
     for i in range(len(station_name_list)):
-        new_dict[station_name_list[i]] = temp_list[i]
-    sorted_dict = {}
-    sorted_keys = sorted(new_dict, key=new_dict.get)
-    # TODO: Check that if station has same name it will arrange alphabetically
-    for i in sorted_keys:
-        sorted_dict[i] = new_dict[i]
+        temp_dict[station_name_list[i]] = temp_list[i]
+    sorted_keys = sorted(temp_dict, key=temp_dict.get)
     # To not make it error, if K more than len of dict, let K = len of dict
-    if K > len(sorted_dict):
-        output_len = len(sorted_dict)
+    if K > len(temp_dict):
+        output_len = len(temp_dict)
     else:
         output_len = K
-    return dict(list(sorted_dict.items())[0: output_len])
+    return sorted_keys[0:output_len]
 
 
 def top_k_max_temp_stations(stations, K):
@@ -225,39 +229,93 @@ def top_k_max_temp_stations(stations, K):
         มี
         """
     # Just copy top_k_min_temp_stations and change some code to get max
-    new_dict = {}
-    # Make list of station and temp from dict
     station_name_list = list(stations.keys())
     temp_list = []
     for name in station_name_list:
         temp_list.append(stations[name]["temp"])
-    # Make a new dict
+    # Make a dict with station and temp
+    temp_dict = {}
     for i in range(len(station_name_list)):
-        new_dict[station_name_list[i]] = temp_list[i]
-    sorted_dict = {}
-    sorted_keys = sorted(new_dict, key=new_dict.get, reverse=True)
-    # TODO: Check that if station has same name it will arrange alphabetically
-    for i in sorted_keys:
-        sorted_dict[i] = new_dict[i]
+        temp_dict[station_name_list[i]] = temp_list[i]
+    sorted_keys = sorted(temp_dict, key=temp_dict.get, reverse=True)
     # To not make it error, if K more than len of dict, let K = len of dict
-    if K > len(sorted_dict):
-        output_len = len(sorted_dict)
+    if K > len(temp_dict):
+        output_len = len(temp_dict)
     else:
         output_len = K
-    return dict(list(sorted_dict.items())[0: output_len])
+    return sorted_keys[0:output_len]
 
 
 def peak_stations(stations):
-    pass
-
+    # Make a station name list
+    station_name_list = list(stations.keys())
+    lat_list = []
+    for name in station_name_list:
+        lat_list.append(stations[name]["lat"])
+    long_list = []
+    for name in station_name_list:
+        long_list.append(stations[name]["long"])
+    # We get a list of lat and long then find a max and min of lat and long and put it in a new variable as a float.
+    lat_max = max(lat_list)
+    lat_min = min(lat_list)
+    long_max = max(long_list)
+    long_min = min(long_list)
+    # For loop to get a station name
+    for i in range(len(station_name_list)):
+        if lat_list[i] == lat_max:
+            lat_max_station = station_name_list[i]
+    for i in range(len(station_name_list)):
+        if lat_list[i] == lat_min:
+            lat_min_station = station_name_list[i]
+    for i in range(len(station_name_list)):
+        if long_list[i] == long_max:
+            long_max_station = station_name_list[i]
+    for i in range(len(station_name_list)):
+        if long_list[i] == long_min:
+            long_min_station = station_name_list[i]
+    return lat_min_station,lat_max_station,long_min_station,long_max_station
 
 def k_nearby_stations(stations, main_station, K):
-    pass
+    station_name_list = list(stations.keys())
+    lat_list = []
+    for name in station_name_list:
+        lat_list.append(stations[name]["lat"])
+    long_list = []
+    for name in station_name_list:
+        long_list.append(stations[name]["long"])
+    # Get main_station lat and long
+    main_station_index = station_name_list.index(main_station)
+    main_station_lat = lat_list[main_station_index]
+    main_station_long = long_list[main_station_index]
+    cal_list = []
+    for i in range(len(station_name_list)):
+        cal_list.append(distance(lat_list[i], long_list[i], main_station_lat, main_station_long))
+    # Make a dict of name and distance
+    name_distance_dict = {}
+    for i in range(len(station_name_list)):
+        name_distance_dict[station_name_list[i]] = cal_list[i]
+    # Delete main_station itself out from a dict
+    del name_distance_dict[main_station]
+    # Sort a dict
+    sorted_keys = sorted(name_distance_dict, key=name_distance_dict.get)
+    # To not make it error, if K more than len of dict, let K = len of dict
+    if K > len(sorted_keys):
+        output_len = len(sorted_keys)
+    else:
+        output_len = K
+    return sorted_keys[0:output_len]
 
 
 def average_temp(stations, names):
-    pass
-
+    station_name_list = list(stations.keys())
+    temp_list = []
+    calculation_list = []
+    for name in station_name_list:
+        temp_list.append(stations[name]["temp"])
+    for station in names:
+        station_index = station_name_list.index(station)
+        calculation_list.append(temp_list[station_index])
+    return sum(calculation_list)/len(calculation_list)
 
 # If you put another line note again I will get Kasumi to 'BanG Dream!' you. (The 3 line is not the same too. WUT)
-# main()
+main()
